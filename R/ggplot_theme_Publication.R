d9030e2b78c05664ec2bc18ad61f3548de1ce13d
Adrian Stier <adrian.stier@gmailcom>
2017-03-30 15:34:43 -0700

    minor edits for ACS's prefs

--- R/ggplot_theme_Publication.R
+++ R/ggplot_theme_Publication.R
@@ -1,8 +1,10 @@
+library(grid)
+library(ggthemes)
+library(ggplot)
 
 
 theme_Publication <- function(base_size=14, base_family="sans") {
-      library(grid)
-      library(ggthemes)
+      
       (theme_foundation(base_size=base_size, base_family=base_family)
        + theme(plot.title = element_text(face = "bold",
                                          size = rel(1.2), hjust = 0.5),
@@ -13,18 +15,20 @@ theme_Publication <- function(base_size=14, base_family="sans") {
                axis.title = element_text(face = "bold",size = rel(1)),
                axis.title.y = element_text(angle=90,vjust =2),
                axis.title.x = element_text(vjust = -0.2),
+               axis.ticks.length =  unit(0.15, "cm"),
                axis.text = element_text(), 
                axis.line.x = element_line(colour="black"),
                axis.line.y = element_line(colour="black"),
                axis.ticks = element_line(),
-               panel.grid.major = element_line(colour="#f0f0f0"),
+               panel.grid.major = element_blank(),
                panel.grid.minor = element_blank(),
                legend.key = element_rect(colour = NA),
-               legend.position = "bottom",
-               legend.direction = "horizontal",
+               legend.position = "right",
+               legend.direction = "vertical",
                legend.key.size= unit(0.2, "cm"),
                legend.margin = unit(0, "cm"),
                legend.title = element_text(face="italic"),
+               legend.text = element_text(size = rel(0.8)),
                plot.margin=unit(c(10,5,5,5),"mm"),
                strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
                strip.text = element_text(face="bold")
@@ -32,14 +36,18 @@ theme_Publication <- function(base_size=14, base_family="sans") {
       
 }
 
-scale_fill_Publication <- function(...){
-      library(scales)
-      discrete_scale("fill","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
-      
-}
 
 scale_colour_Publication <- function(...){
       library(scales)
       discrete_scale("colour","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
       
 }
+
+#Test
+data(cars)
+
+ggplot(mtcars, aes(mpg,disp,color=factor(carb),size=hp))+
+  geom_point(alpha=0.7) + labs(title="Bubble Plot")+
+  scale_size_continuous(range = c(3,10))+
+  scale_colour_Publication()+
+  theme_Publication()
